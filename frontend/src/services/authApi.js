4e876e0692d9809ff86add69e4bd9f2439a391a4
Hailemari <hailemariamkefale19@gmail.com>
2024-06-17 11:16:15 +0300

    updated

--- frontend/src/services/authApi.js
+++ frontend/src/services/authApi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const BASE_URL = 'https://manage-content.onrender.com/';
-const BASE_URL = 'http://localhost:5000/';
+const BASE_URL = 'https://manage-content.onrender.com/';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
